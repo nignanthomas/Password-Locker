e47dec8fe774e87fd0b37e553ec34272e628b081
nignanthomas <nignanthomas@gmail.com>
2018-11-02 13:17:54 +0300

    define the test_init function in passlock_test.py -- OK

--- passlock_test.py
+++ passlock_test.py
@@ -15,3 +15,23 @@ class TestUser(unittest.TestCase):
         Set up method to run before each test cases.
         '''
         self.new_user = User("nignanthomas","blablabla",) # create user object
+    # end setUp
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.username,"nignanthomas")
+        self.assertEqual(self.new_user.password,"blablabla")
+
+    # end test_init
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
