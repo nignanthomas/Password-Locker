6af65df44fd50b2b1f10e47752ec438d57073bec
nignanthomas <nignanthomas@gmail.com>
2018-11-02 12:37:23 +0300

    define the setUp function in passlock_test.py

---
+++ passlock_test.py
@@ -0,0 +1,17 @@
+import unittest     #import unittest module
+from user import User   #import the user class
+
+
+class TestUser(unittest.TestCase):
+    """
+    Test class that defines test cases for the user class behavioursself.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    """
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("nignanthomas","blablabla",) # create user object
