b6a8181666c222cf40ef071549a35720183c0b34
nignanthomas <nignanthomas@gmail.com>
2018-11-02 12:36:10 +0300

    define the class User in user.py

---
+++ user.py
@@ -0,0 +1,19 @@
+class User:
+    """
+    Class that generates new instances of users
+    """
+
+    user_list = []  #Empty user user_list
+
+
+
+    def __init__(self, username, password):
+        """
+        __init__ method that helps us define properties for our objects.
+        """
+
+        self.username = username
+        self.password = password
+        self.user_creds = []
+
+    #end init
